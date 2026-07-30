103635d84d47619e5c0c324135d11aff46033cf9
satyamanas <manassatya13@gmail.com>
2026-07-30 10:56:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,15 +43,15 @@ options = ["Delhi", "Mumbai", "Pune", "Banglore",
            "Gurugram/Gurgaon"]
 location = st.sidebar.multiselect("Select Location",
                                   options = options)
-
+profile_op = ["Data Analysts", "AI Engineer","Gen AI Developer", "Full-Stack Dev",
+              "Data Scientist"]
 
 profile = st.sidebar.multiselect("Select Job Profile",
                                  options = profile_op)
 
 
 
-profile_op = ["Data Analysts", "AI Engineer","Gen AI Developer", "Full-Stack Dev",
-              "Data Scientist"]
+
 #model creation
 
 model = ChatGoogleGenerativeAI(
